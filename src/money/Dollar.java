a7dc11574cbe9a91c28df7166b1414ceddbbc983
sarastier <sarastier@gmail.com>
2018-03-22 11:44:27 +0900

    p15 コンパイル通し

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -5,7 +5,8 @@ public class Dollar {
     Dollar(int amount) {
         this.amount = amount;
     }
-    void times(int multiplier) {
+    Dollar times(int multiplier) {
         amount *= multiplier;
+        return null;
     }
 }
