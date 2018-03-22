fa55845504b2083d5db0f27b45ad5b26bb16b3b3
sarastier <sarastier@gmail.com>
2018-03-22 17:43:49 +0900

    p36 Dollar.equals()をMoneyに引き上げた

--- src/money/Money.java
+++ src/money/Money.java
@@ -2,4 +2,10 @@ package money;
 
 public class Money {
     protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return this.amount == money.amount;
+    }
+
 }
