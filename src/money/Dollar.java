f141b8736c84840407207978dcfe2d18f59fa56b
sarastier <sarastier@gmail.com>
2018-03-22 11:46:40 +0900

    p15 Dollar.timesメソッドが計算済み金額のDollarオブジェクトを返すように変更

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -6,7 +6,6 @@ public class Dollar {
         this.amount = amount;
     }
     Dollar times(int multiplier) {
-        amount *= multiplier;
-        return null;
+        return new Dollar(amount * multiplier);
     }
 }
