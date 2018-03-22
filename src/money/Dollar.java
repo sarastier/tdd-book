a7613af76c9feaa9d4500fb37530f88d9c6344a1
sarastier <sarastier@gmail.com>
2018-03-22 12:11:58 +0900

    p20 equalsメソッドがどのオブジェクトでも受け入れるようにI/Fを戻した

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -2,10 +2,17 @@ package money;
 
 public class Dollar {
     int amount;
+
     Dollar(int amount) {
         this.amount = amount;
     }
+
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return this.amount == dollar.amount;
     }
 }
