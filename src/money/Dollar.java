94d260454e22cfdcc32e1cf105a51f53dcbba83b
sarastier <sarastier@gmail.com>
2018-03-22 11:36:04 +0900

    p11 refactor

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -1,9 +1,11 @@
 package money;
 
 public class Dollar {
-    int amount = 10;
+    int amount;
     Dollar(int amount) {
+        this.amount = amount;
     }
-    void times(int multipilier) {
+    void times(int multiplier) {
+        amount *= multiplier;
     }
 }
