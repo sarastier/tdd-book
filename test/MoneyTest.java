f37efe919b1a5e91d6d8022156e7c488b616d5ab
sarastier <sarastier@gmail.com>
2018-03-23 11:29:35 +0900

    p77 Francクラスに関するテスト内容を確認の上、クラスごと削除

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -18,9 +18,6 @@ public class MoneyTest {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -38,8 +35,4 @@ public class MoneyTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-    }
 }
