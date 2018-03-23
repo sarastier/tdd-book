67d122c540abccfc865f66c54af98e6526a44658
sarastier <sarastier@gmail.com>
2018-03-23 10:46:10 +0900

    p58 currecyを親クラスに引き上げた

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -1,15 +1,10 @@
 package money;
 
 public class Dollar extends Money {
-    private String currency;
 
     Dollar(int amount) {
         this.amount = amount;
         currency = "USD";
-    }
-
-    String currency() {
-        return currency;
     }
 
     Money times(int multiplier) {
--- src/money/Franc.java
+++ src/money/Franc.java
@@ -1,15 +1,10 @@
 package money;
 
 public class Franc extends Money {
-    private String currency;
 
     Franc(int amount) {
         this.amount = amount;
         currency = "CHF";
-    }
-
-    String currency() {
-        return currency;
     }
 
     Money times(int multiplier) {
