aae650c6776ed8675c93a19c62d67f4f23c584dc
sarastier <sarastier@gmail.com>
2018-03-23 10:22:17 +0900

    p49 MoneyクラスにDollarクラスのファクトリメソッドを追加

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,12 +1,18 @@
 package money;
 
-public class Money {
+abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return this.amount == money.amount
             && getClass().equals(money.getClass());
+    }
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
     }
 
 }
