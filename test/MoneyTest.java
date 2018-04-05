fa737fc926434a9a6aea95b6f265c85e085b8ef2
sarastier <sarastier@gmail.com>
2018-04-05 16:22:23 +0900

    p117 Expression.plusの呼び出しをしているが、ないのでテストが通らない

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -79,7 +79,7 @@ public class MoneyTest {
 
     @Test
     public void testMixedAddition() {
-        Money fiveBucks = Money.dollar(5);
+        Expression fiveBucks = Money.dollar(5);
         Expression tenFrancs = Money.franc(10);
         Bank bank = new Bank();
 
