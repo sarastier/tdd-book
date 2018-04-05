92eb3161cce4b3947b33a8ee8af86f3840191ff8
sarastier <sarastier@gmail.com>
2018-04-05 15:23:14 +0900

    p92 Band.reduceの微妙な実装を書いた

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -1,7 +1,9 @@
 package money;
 
 class Bank {
-    Money reduce(Expression souce, String to) {
-        return Money.dollar(10);
+    Money reduce(Expression source, String to) {
+        Sum sum = (Sum) source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, to);
     }
 }
