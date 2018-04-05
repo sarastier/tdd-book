1c3f3cfc43fbdbaacca61e4961e717cd36396aba
sarastier <sarastier@gmail.com>
2018-04-05 16:19:36 +0900

    p116 Money.timesの引数の型も抽象度を上げた

--- src/money/Money.java
+++ src/money/Money.java
@@ -9,7 +9,7 @@ class Money implements Expression {
         this.currency = currency;
     }
 
-    Money times(int multiplier) {
+    Expression times(int multiplier) {
         return new Money(amount * multiplier,currency);
     }
 
