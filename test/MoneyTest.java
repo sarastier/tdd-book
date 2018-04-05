fbfa0873a19054654765e4b7575c2b0d1dedbc06
sarastier <sarastier@gmail.com>
2018-04-05 18:13:20 +0900

    p127 Expression.times()を実装

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -109,7 +109,7 @@ public class MoneyTest {
         Bank bank = new Bank();
 
         bank.addRate("CHF", "USD", 2);
-        Expression sum = new Sum(fiveBucks, tenFrancs).times(fiveBucks);
+        Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
         Money result = bank.reduce(sum, "USD");
 
         assertEquals(Money.dollar(20), result);
