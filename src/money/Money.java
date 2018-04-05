dad380f2ff657af51e97439017d9ef3097147ed1
sarastier <sarastier@gmail.com>
2018-04-05 15:33:58 +0900

    p95 Expressionがreduceをサポートするようにリファクタ

--- src/money/Money.java
+++ src/money/Money.java
@@ -17,6 +17,10 @@ class Money implements Expression {
         return new Sum(this, addend);
     }
 
+    public Money reduce(String to) {
+        return this;
+    }
+
     public String currency() {
         return currency;
     }
